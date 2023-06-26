0416e69d10ea62d658e45e27f9fb1590b29064ec
trungbaotran1701 <trungbaotran1701@gmail.com>
2023-06-27 03:12:52 +0700

    done invite but still not instant display in member list

--- webapp/src/middleware/data/user.tsx
+++ webapp/src/middleware/data/user.tsx
@@ -1,26 +1,32 @@
-import { finishLoading, startLoading } from '@redux/reducers/ui.reducer';
-import { supabase } from '@utils/supabaseClient';
+import { finishLoading, startLoading } from "@redux/reducers/ui.reducer";
+import { supabase } from "@utils/supabaseClient";
 
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const inviteUserByEmail = async ({
-  email, dispatch, onSuccess, onError = (e:any) => { console.error(e); },
+  email,
+  dispatch,
+  onSuccess,
+  onError = (e: any) => {
+    console.error(e);
+  },
 }: {
-  email: string,
-  dispatch: any,
-  onSuccess: (data:any) => void,
-  onError?: (error:any) => void,
+  email: string;
+  dispatch: any;
+  onSuccess: (data: any) => void;
+  onError?: (error: any) => void;
 }) => {
   dispatch(startLoading({}));
   // TODO: email validate!
   // TODO: move this to the edge function
   try {
-    const url = 'https://uafmqopjujmosmilsefw.supabase.co/functions/v1/invite-user-email';
+    const url =
+      "https://uafmqopjujmosmilsefw.supabase.co/functions/v1/invite-user-email";
     const response = await fetch(url, {
-      method: 'POST',
+      method: "POST",
       headers: {
         authorization: `Bearer ${supabaseAnonKey}`,
-        'content-type': 'application/json',
+        "content-type": "application/json",
       },
       body: JSON.stringify({
         email,
@@ -34,15 +40,23 @@ export const inviteUserByEmail = async ({
   dispatch(finishLoading({}));
 };
 export const queryUserByEmail = async ({
-  email, dispatch, onSuccess, onError = (e:any) => { console.error(e); },
+  email,
+  dispatch,
+  onSuccess,
+  onError = (e: any) => {
+    console.error(e);
+  },
 }: {
-  email: string,
-  dispatch: any,
-  onSuccess: (data:any) => void,
-  onError?: (error:any) => void,
+  email: string;
+  dispatch: any;
+  onSuccess: (data: any) => void;
+  onError?: (error: any) => void;
 }) => {
   dispatch(startLoading({}));
-  const { data, error } = await supabase.from('profile').select('id, email, full_name').eq('email', email);
+  const { data, error } = await supabase
+    .from("profile")
+    .select("id, email, full_name")
+    .eq("email", email);
   if (error) {
     onError(error);
   } else {
@@ -51,42 +65,70 @@ export const queryUserByEmail = async ({
 };
 
 export const addMemberToOrg = async ({
-  data, dispatch, onSuccess, onError = (e:any) => { console.error(e); },
+  userOrgInfo,
+  dispatch,
+  onSuccess,
+  onError = (e: any) => {
+    console.error(e);
+  },
 }: {
-  data: any,
-  dispatch: any,
-  onSuccess: (data:any) => void,
-  onError?: (error:any) => void,
+  userOrgInfo: any;
+  dispatch: any;
+  onSuccess: () => void;
+  onError?: (error: any) => void;
 }) => {
-  const { email } = data;
   dispatch(startLoading({}));
+  const { data, error } = await supabase.from("user_org").insert(userOrgInfo);
+  if (error) {
+    onError(error);
+  } else {
+    onSuccess();
+  }
   dispatch(finishLoading({}));
 };
 
 export const inviteExistingMember = async ({
-  data, dispatch, onSuccess, onError = (e:any) => { console.error(e); },
+  data,
+  dispatch,
+  onSuccess,
+  onError = (e: any) => {
+    console.error(e);
+  },
 }: {
-  data: any,
-  dispatch: any,
-  onSuccess: (data:any) => void,
-  onError?: (error:any) => void,
+  data: any;
+  dispatch: any;
+  onSuccess: () => void;
+  onError?: (error: any) => void;
 }) => {
-  const { to_email, inviter, full_name, org_title } = data; //eslint-disable-line
+  const { to_email, inviter, full_name, org_title, org_id, id_user } = data; //eslint-disable-line
   dispatch(startLoading({}));
   try {
-    const url = 'https://uafmqopjujmosmilsefw.supabase.co/functions/v1/send-email';
+    const url =
+      "https://uafmqopjujmosmilsefw.supabase.co/functions/v1/send-email";
     const response = await fetch(url, {
-      method: 'POST',
+      method: "POST",
       headers: {
         authorization: `Bearer ${supabaseAnonKey}`,
-        'content-type': 'application/json',
+        "content-type": "application/json",
       },
       body: JSON.stringify({
-        to_email, inviter, full_name, org_title,
+        to_email,
+        inviter,
+        full_name,
+        org_title,
       }),
     });
     const result = await response.json();
-    onSuccess(result);
+    if (result) {
+      const userOrgInfo = {
+        org_id: org_id,
+        user_id: id_user,
+        role: "MEMBER",
+      };
+      await addMemberToOrg({ userOrgInfo, dispatch, onSuccess, onError });
+    } else {
+      onError(new Error("Cannot invite user"));
+    }
   } catch (error) {
     onError(error);
   }
