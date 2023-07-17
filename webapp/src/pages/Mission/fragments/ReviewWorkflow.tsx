145c0a35fd2244126bb0eea764d401e04b2ce00c
Hieu <trunghieubui88@gmail.com>
2023-07-17 15:12:57 +0700

    fix editable

--- webapp/src/pages/Mission/fragments/ReviewWorkflow.tsx
+++ webapp/src/pages/Mission/fragments/ReviewWorkflow.tsx
@@ -8,7 +8,7 @@ import moment from 'moment';
 import { useState } from 'react';
 import ExternalLinkIcon from '@assets/icons/svg-icons/ExternalLinkIcon';
 import { useNavigate, useParams } from 'react-router-dom';
-import { IWorkflowVersion } from '@types';
+import { GraphViewMode, IWorkflowVersion } from '@types';
 
 const getSummary = (selected: ICheckPoint) => {
   const machine: any = getVoteMachine(selected?.vote_machine_type || '');
@@ -28,19 +28,19 @@ const ReviewWorkflow = ({
   const navigate = useNavigate();
   const { orgIdString, workflowIdString } = useParams();
   return (
-    <Space size="large" direction="vertical" className="w-full">
-      <p className="text-[28px] text-grey-version-7 font-semibold">
+    <Space size='large' direction='vertical' className='w-full'>
+      <p className='text-[28px] text-grey-version-7 font-semibold'>
         New mission from Workflow
       </p>
-      <Space direction="vertical" size="small" className="w-full">
-        <div className="flex gap-2 items-center">
-          <p className="text-[#575655] text-base">From Workflow</p>
-          <div className="font-semibold pt-[0.9px] flex items-center">
-            <span className="mr-2">{`${currentWorkflowVersion.title}`}</span>
+      <Space direction='vertical' size='small' className='w-full'>
+        <div className='flex gap-2 items-center'>
+          <p className='text-[#575655] text-base'>From Workflow</p>
+          <div className='font-semibold pt-[0.9px] flex items-center'>
+            <span className='mr-2'>{`${currentWorkflowVersion.title}`}</span>
             {/* TODO: display of tag is inconsistent */}
             <Tag>{`${currentWorkflowVersion.version}`}</Tag>
             <span
-              className="cursor-pointer"
+              className='cursor-pointer'
               onClick={() =>
                 navigate(`/${orgIdString}/edit/${workflowIdString}`)
               }
@@ -49,20 +49,20 @@ const ReviewWorkflow = ({
             </span>
           </div>
         </div>
-        <div className="flex gap-2 items-center">
-          <p className="text-[#575655] text-base">Created on</p>
-          <p className="font-semibold pt-[0.9px]">
+        <div className='flex gap-2 items-center'>
+          <p className='text-[#575655] text-base'>Created on</p>
+          <p className='font-semibold pt-[0.9px]'>
             {`${moment(currentWorkflowVersion.created_at).format(
               'DD MMM YYYY'
             )}`}
           </p>
         </div>
         <div
-          className="flex gap-2 items-center w-full"
+          className='flex gap-2 items-center w-full'
           style={{ height: '400px' }}
         >
           <DirectedGraph
-            editable={false}
+            viewMode={GraphViewMode.VIEW_ONLY}
             data={currentWorkflowVersion.data}
             onNodeClick={(e: any, node: any) => {
               const { checkpoints } = currentWorkflowVersion.data;
