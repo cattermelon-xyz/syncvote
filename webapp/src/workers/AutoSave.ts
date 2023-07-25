52954152836da41e3f3bf7e79455bb0d466bfc16
Hieu <trunghieubui88@gmail.com>
2023-07-25 16:06:47 +0700

    add dummy import to prevent build from failure

--- webapp/src/workers/AutoSave.ts
+++ webapp/src/workers/AutoSave.ts
@@ -1,4 +1,7 @@
 /* eslint-disable no-restricted-globals */
+import React from 'react';
+// Dummy import to prevent build from failure
+
 self.onmessage = (message) => {
   sleep(10000);
   self.postMessage('Done!');
