54f203883d0f43b8a53914ee129add7e757308b2
Hieu <trunghieubui88@gmail.com>
2023-08-03 19:59:08 +0700

    mave sleep func

--- webapp/public/workers/AutoSave.ts
+++ webapp/public/workers/AutoSave.ts
@@ -2,15 +2,15 @@
 import React from 'react';
 // Dummy import to prevent build from failure
 
-self.onmessage = (message) => {
-  sleep(10000);
-  self.postMessage('Done!');
-};
-
 const sleep = (milliseconds: number) => {
   const date = Date.now();
   let currentDate;
   do {
     currentDate = Date.now();
   } while (currentDate - date < milliseconds);
+};
+
+self.onmessage = (message) => {
+  sleep(10000);
+  self.postMessage('Done!');
 };
