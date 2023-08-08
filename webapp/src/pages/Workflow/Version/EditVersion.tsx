219a4af57f72fc63246e0f68ddef93cbbab1b11f
onspeedhp <chaukhac4@gmail.com>
2023-08-08 18:04:12 +0700

    fix error lodash

--- webapp/src/pages/Workflow/Version/EditVersion.tsx
+++ webapp/src/pages/Workflow/Version/EditVersion.tsx
@@ -4,7 +4,6 @@ import {
   SaveOutlined,
 } from '@ant-design/icons';
 import EditIcon from '@assets/icons/svg-icons/EditIcon';
-import _ from 'lodash';
 import {
   DirectedGraph,
   defaultLayout,
@@ -33,6 +32,36 @@ import {
 import { AuthContext } from '@layout/context/AuthContext';
 import Header from './fragment/Header';
 import EditWorkflow from '../BluePrint/fragment/EditWorkflow';
+
+function deepEqual(object1: any, object2: any): boolean {
+  if (object1 === object2) {
+    return true;
+  }
+
+  if (
+    typeof object1 !== 'object' ||
+    object1 === null ||
+    typeof object2 !== 'object' ||
+    object2 === null
+  ) {
+    return false;
+  }
+
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (!keys2.includes(key) || !deepEqual(object1[key], object2[key])) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 const extractVersion = ({
   workflows,
@@ -116,7 +145,8 @@ export const EditVersion = () => {
     });
     setVersion(extractedVersion);
 
-    if (_.isEqual(version.data, emptyStage)) {
+    if (deepEqual(version.data, emptyStage)) {
+      
       setUploadImage(true);
     }
 
