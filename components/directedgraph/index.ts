ac59a22d32d6b5f0034433c8970558451169c59f
trungbaotran1701 <trungbaotran1701@gmail.com>
2023-09-06 17:14:29 +0700

    change name

--- components/directedgraph/index.ts
+++ components/directedgraph/index.ts
@@ -1,18 +1,18 @@
-import { ICheckPoint as icp } from './src/DirectedGraph/interface';
+import { ICheckPoint as icp } from './src/DirectedGraphComponent/interface';
 // TODO: ICheckPoint should be defined here, then import in @types and rexport!
-export * from './src/DirectedGraph/DirectedGraph';
-export * from './src/DirectedGraph/voteMachine';
-export * from './src/DirectedGraph/renderVoteMachineConfigPanel';
-export * from './src/DirectedGraph/empty';
-export * from './src/DirectedGraph/interface';
-export * from './src/DirectedGraph/utils';
-export * from './src/DirectedGraph/components/NavConfigPanel';
-export { default as NavConfigPanel } from './src/DirectedGraph/components/NavConfigPanel';
-export { default as CollapsiblePanel } from './src/DirectedGraph/components/CollapsiblePanel';
-export { default as SideNote } from './src/DirectedGraph/components/SideNote';
-export { default as ResultCalculator } from './src/DirectedGraph/components/ResultCalculator';
-export { default as TimelockPanel } from './src/DirectedGraph/components/TimelockPanel';
-export { default as TokenInput } from './src/DirectedGraph/components/TokenInput';
-export { default as NumberWithPercentageInput } from './src/DirectedGraph/components/NumberWithPercentageInput';
+export * from './src/DirectedGraphComponent/DirectedGraph';
+export * from './src/DirectedGraphComponent/voteMachine';
+export * from './src/DirectedGraphComponent/renderVoteMachineConfigPanel';
+export * from './src/DirectedGraphComponent/empty';
+export * from './src/DirectedGraphComponent/interface';
+export * from './src/DirectedGraphComponent/utils';
+export * from './src/DirectedGraphComponent/components/NavConfigPanel';
+export { default as NavConfigPanel } from './src/DirectedGraphComponent/components/NavConfigPanel';
+export { default as CollapsiblePanel } from './src/DirectedGraphComponent/components/CollapsiblePanel';
+export { default as SideNote } from './src/DirectedGraphComponent/components/SideNote';
+export { default as ResultCalculator } from './src/DirectedGraphComponent/components/ResultCalculator';
+export { default as TimelockPanel } from './src/DirectedGraphComponent/components/TimelockPanel';
+export { default as TokenInput } from './src/DirectedGraphComponent/components/TokenInput';
+export { default as NumberWithPercentageInput } from './src/DirectedGraphComponent/components/NumberWithPercentageInput';
 
 export type { icp as ICheckPoint };
