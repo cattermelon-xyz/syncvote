944b3245f22ccc91a291194825d4715e7d37fd93
onspeedhp <chaukhac4@gmail.com>
2023-09-09 03:18:36 +0700

    Add dal to utils

---
+++ utils/src/dal/dal.ts
@@ -0,0 +1,83 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { checkShouldCache } from '../function/checkShouldCache';
+
+export interface ConfigInfo {
+  dalFunction: any;
+  reduxObjectPath?: string;
+}
+
+interface useGetDataHookProps<T> {
+  params?: T;
+  configInfo: ConfigInfo;
+  cacheOption?: boolean;
+  start?: boolean;
+}
+
+export function useGetDataHook<T>({
+  params,
+  configInfo,
+  cacheOption = true,
+  start,
+}: useGetDataHookProps<T>) {
+  const reduxDataReturn = useSelector(
+    (state: any) => state[configInfo.reduxObjectPath!]
+  );
+
+  const { lastFetch } = reduxDataReturn;
+
+  const dispatch = useDispatch();
+  const [data, setData] = useState<any>(null);
+  const [error, setError] = useState<any>(null);
+
+  const shouldCache = checkShouldCache({
+    cacheOption: cacheOption,
+    lastFetch: lastFetch,
+  });
+
+  useEffect(() => {
+    if (start !== false) {
+      if (typeof configInfo.dalFunction === 'function') {
+        configInfo.dalFunction({
+          params: params,
+          dispatch: dispatch,
+          shouldCache: shouldCache,
+          onSuccess: (data: any) => {
+            setData(data);
+          },
+          onError: (error: any) => {
+            setError(error);
+          },
+          reduxDataReturn: structuredClone(reduxDataReturn),
+        });
+      }
+    }
+  }, [start, reduxDataReturn]);
+
+  return { data, error };
+}
+
+export async function useSetData<T>({
+  onSuccess,
+  onError,
+  params,
+  configInfo,
+  dispatch,
+}: {
+  onSuccess?: (data: any) => void;
+  onError?: (error: any) => void;
+  params?: any;
+  configInfo?: any;
+  dispatch?: any;
+}) {
+  const clonedParams: T | undefined = structuredClone(params);
+
+  if (typeof configInfo.dalFunction === 'function') {
+    await configInfo.dalFunction({
+      params: clonedParams,
+      dispatch,
+      onSuccess: onSuccess,
+      onError: onError,
+    });
+  }
+}
