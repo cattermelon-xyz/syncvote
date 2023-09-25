4bc4fe69c2fb2b82875be98ce3d14cb087edf389
Loc Ng <locnguyen.isme@gmail.com>
2023-09-25 13:52:12 +0700

    fix bug Redirect edit link to public link if user does not have permission to edit

--- apps/syncvote/webapp/src/routing/RequireAuth.tsx
+++ apps/syncvote/webapp/src/routing/RequireAuth.tsx
@@ -5,12 +5,14 @@ import { Navigate, useLocation } from 'react-router-dom';
 function RequireAuth({ children }: { children: JSX.Element }) {
   const { isAuth } = useContext(AuthContext);
   let location = useLocation();
+
   if (!isAuth) {
-    // Redirect them to the /login page, but save the current location they were
+    // Redirect them to the workflow public view page, but save the current location they were
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
     // than dropping them off on the home page.
-    return <Navigate to='/login' state={{ from: location }} />;
+    const redirectToPublic = `/public${location.pathname}`;
+    return <Navigate to={redirectToPublic} state={{ from: location }} />;
   }
 
   return children;
