80f24e244cf6e3f4fc64491dc8249096df3dadfe
trungbaotran1701 <trungbaotran1701@gmail.com>
2023-10-03 11:58:18 +0700

    fix bug search

--- apps/syncvote/webapp/src/pages/Organization/Explore.tsx
+++ apps/syncvote/webapp/src/pages/Organization/Explore.tsx
@@ -13,6 +13,7 @@ import { ReloadOutlined } from '@ant-design/icons';
 const Home: React.FC = () => {
   const dispatch = useDispatch();
   const [OrgsData, setOrgsData] = useState<any[]>([]);
+  const [isSearching, setIsSearching] = useState(false);
   const [page, setPage] = useState(0);
 
   const getFromAndTo = () => {
@@ -49,10 +50,8 @@ const Home: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    console.log("OrgsData", OrgsData)
-  }, [
-    OrgsData
-  ])
+    console.log('OrgsData', OrgsData);
+  }, [OrgsData]);
 
   return (
     <div className='w-[800px] flex flex-col gap-y-14'>
@@ -63,29 +62,36 @@ const Home: React.FC = () => {
         <SearchWithTag
           tagTo={TagObject.ORGANIZATION}
           onResult={(result: any) => {
-            // setOrgsData(result);
+            if (result) {
+              setOrgsData(result);
+              setIsSearching(true);
+            }
           }}
           showSearchTag={false}
         />
         <ListItem
           // handleSort={handleSortSpaceDetail}
           items={
-            (OrgsData) &&
-            (OrgsData).map((OrgData, index) => (
+            OrgsData &&
+            OrgsData.map((OrgData, index) => (
               <OrgCard key={index} orgData={OrgData} />
             ))
           }
           columns={{ xs: 2, md: 3, xl: 4, '2xl': 4 }}
         />
-        <div className='w-full flex justify-center items-center'>
-          <Button
-            onClick={fetchData}
-            className='mt-4'
-            icon={<ReloadOutlined />}
-          >
-            View More
-          </Button>
-        </div>
+        {isSearching ? (
+          <div></div>
+        ) : (
+          <div className='w-full flex justify-center items-center'>
+            <Button
+              onClick={fetchData}
+              className='mt-4'
+              icon={<ReloadOutlined />}
+            >
+              View More
+            </Button>
+          </div>
+        )}
       </section>
     </div>
   );
