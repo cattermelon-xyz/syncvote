a6f3e2829252400c43a083514f43cf718c56cea7
onspeedhp <chaukhac4@gmail.com>
2023-10-19 00:49:41 +0700

    Change cors

--- apps/syncvote/service/node/src/server.js
+++ apps/syncvote/service/node/src/server.js
@@ -11,13 +11,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.use(
-  cors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 app.use(bodyParser.json());
