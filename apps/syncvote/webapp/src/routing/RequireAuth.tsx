2c47081c291afa090b2585fc825e082627156fb1
Lê Khắc Châu <chaukhac4@gmail.com>
2023-10-23 11:02:35 +0700

    Change show up template desc to workflow desc (#352)

--- apps/syncvote/webapp/src/routing/RequireAuth.tsx
+++ apps/syncvote/webapp/src/routing/RequireAuth.tsx
@@ -6,15 +6,14 @@ function RequireAuth({ children }: { children: JSX.Element }) {
   const { isAuth } = useContext(AuthContext);
   let location = useLocation();
   const navigate = useNavigate();
-  
+
   useEffect(() => {
-    if (isAuth) { // Check if the user is authenticated
+    if (isAuth) {
+      // Check if the user is authenticated
       const lastVisitedPage = sessionStorage.getItem('lastVisitedPage');
       if (lastVisitedPage) {
         navigate(lastVisitedPage); // Navigate to the stored page
         sessionStorage.removeItem('lastVisitedPage'); // Clear the stored page
-      } else {
-        navigate('/'); // Default redirection if no stored page found
       }
     }
   }, [isAuth, navigate]);
@@ -29,6 +28,5 @@ function RequireAuth({ children }: { children: JSX.Element }) {
 
   return children;
 }
-
 
 export default RequireAuth;
