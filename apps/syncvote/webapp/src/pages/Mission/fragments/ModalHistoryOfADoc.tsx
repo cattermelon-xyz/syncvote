b1dd675a4ddb188c44974e2c219237440aec40c1
trungbaotran1701 <96670624+trungbaotran1701@users.noreply.github.com>
2023-10-26 19:15:46 +0700

    Fix some logic for doc input (#382)
    * console log data
    
    * show lasttime in proposal document and fix add new edit append for docinput

--- apps/syncvote/webapp/src/pages/Mission/fragments/ModalHistoryOfADoc.tsx
+++ apps/syncvote/webapp/src/pages/Mission/fragments/ModalHistoryOfADoc.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Modal, Card } from 'antd';
-import { useDispatch } from 'react-redux';
-import { queryDocInput } from '@dal/data';
+import { Modal, Card, Select, Empty } from 'antd';
 import parse from 'html-react-parser';
+import { formatDate } from '@utils/helpers';
 
 interface Props {
   open: boolean;
   onClose: () => void;
   docTitle?: string;
   versionOfADoc?: any;
+  dataOfAllDocs?: any;
 }
 
 const ModalHistoryOfADoc: React.FC<Props> = ({
@@ -16,36 +16,48 @@ const ModalHistoryOfADoc: React.FC<Props> = ({
   onClose,
   docTitle,
   versionOfADoc,
+  dataOfAllDocs,
 }) => {
-  const dispatch = useDispatch();
   const [dataVersionOfDoc, setDataVersionOfDoc] = useState<any[]>([]);
   const [contentOfVersion, setContentOfVersion] = useState<string>();
+  const [optionsData, setOptionsData] = useState<any[]>([]);
 
   useEffect(() => {
-    console.log('versionOfADoc', versionOfADoc);
-    if (versionOfADoc) {
-      versionOfADoc?.map((versionItem: any) => {
-        const idDocInput = versionItem[Object.keys(versionItem)[0]];
-        queryDocInput({
-          idDocInput,
-          onSuccess: (data: any) => {
-            setDataVersionOfDoc((prevData) => [...prevData, data]);
-          },
-          onError: (error) => {
-            Modal.error({
-              title: 'Error',
-              content: error,
-            });
-          },
-          dispatch,
-        });
-      });
+    if (versionOfADoc && dataOfAllDocs) {
+      const ids = versionOfADoc.flatMap((doc: any) => Object.values(doc));
+
+      const filteredData = dataOfAllDocs
+        .filter((doc: any) => ids.includes(doc.id))
+        .sort(
+          (a: any, b: any) =>
+            new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+        );
+
+      setDataVersionOfDoc(filteredData);
     }
-  }, [versionOfADoc]);
+  }, [versionOfADoc, dataOfAllDocs]);
 
   useEffect(() => {
-    console.log('dataVersionOfDoc', dataVersionOfDoc);
+    if (dataVersionOfDoc) {
+      const options = dataVersionOfDoc?.map((version: any, index: any) => {
+        return {
+          label:
+            index !== 0
+              ? formatDate(version?.created_at)
+              : `${formatDate(version?.created_at)} (Last version)`,
+          value: version.content,
+        };
+      });
+
+      setOptionsData(options);
+    }
   }, [dataVersionOfDoc]);
+
+  useEffect(() => {
+    if (optionsData && optionsData.length > 0) {
+      setContentOfVersion(optionsData[0].value);
+    }
+  }, [optionsData]);
 
   const handleOk = async () => {
     onClose();
@@ -61,24 +73,24 @@ const ModalHistoryOfADoc: React.FC<Props> = ({
         onCancel={() => {
           onClose();
         }}
-        width={1000}
+        width={670}
       >
-        {dataVersionOfDoc && (
-          <div className='flex w-full gap-3'>
-            <Card className='w-4/5'>
-              {contentOfVersion && <div>{parse(contentOfVersion)}</div>}
-            </Card>
-            <Card className='w-1/5'>
-              {dataVersionOfDoc.map((aVersionData: any, index: any) => (
-                <p
-                  key={index}
-                  onClick={() => setContentOfVersion(aVersionData?.content)}
-                >
-                  {aVersionData?.created_at}
-                </p>
-              ))}
-            </Card>
-          </div>
+        {dataVersionOfDoc.length === 0 ? (
+          <Empty />
+        ) : (
+          optionsData.length > 0 && (
+            <div className='flex flex-col w-full gap-3'>
+              <Select
+                defaultValue={optionsData[0]?.value}
+                style={{ width: '50%' }}
+                onChange={(value) => setContentOfVersion(value)}
+                options={optionsData}
+              />
+              <Card className='w-full max-h-[486px] overflow-y-scroll bg-[#F6F6F6]'>
+                {contentOfVersion && <div>{parse(contentOfVersion)}</div>}
+              </Card>
+            </div>
+          )
         )}
       </Modal>
     </>
