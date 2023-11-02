c9e0c4b73e0d42f417e085d0e8184776510a10a6
onspeedhp <chaukhac4@gmail.com>
2023-11-02 15:11:04 +0700

    Update

--- apps/syncvote/service/node/test/server.test.js
+++ apps/syncvote/service/node/test/server.test.js
@@ -2,8 +2,23 @@ const expect = require('chai').expect;
 const { supabase } = require('../src/configs/supabaseClient');
 
 describe('Testing voting function', function () {
+  const email = 'chaukhac4@gmail.com';
+  const misisonId = 1;
   it('1. The side length of the Cube', async function (done) {
     const { data, error } = await supabase.from('mission').select().csv();
+
+    await fetch(`${process.env.BACKEND_API}/vote/create`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        identify: email,
+        option: [1],
+        voting_power: 1,
+        mission_id: newMission[0].id,
+      }),
+    });
     done();
   });
 });
