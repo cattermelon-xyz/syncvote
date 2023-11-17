b0c9070f498156d4cd23d0b8e73bb5ce6fe0483d
onspeedhp <chaukhac4@gmail.com>
2023-11-17 15:27:23 +0700

    Update supabase key

--- apps/syncvote/service/node/src/configs/supabaseClient.js
+++ apps/syncvote/service/node/src/configs/supabaseClient.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 // const supabaseUrl = process.env.SUPABASE_URL;
 // const supabaseAnonKey = process.env.SUPABASE_KEY;
 
-const supabaseUrl = process.env.SUPABASE_URL_TEST;
-const supabaseAnonKey = process.env.SUPABASE_KEY_TEST;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_KEY;
 
 const supabaseClient = {
   supabase: createClient(supabaseUrl, supabaseAnonKey),
