9d9a562ef10ffcf6bab1478a32737c6f87ef5c67
onspeedhp <chaukhac4@gmail.com>
2023-12-08 13:15:18 +0700

    Add handle logic of EndNode

--- apps/syncvote/service/node/src/functions/index.js
+++ apps/syncvote/service/node/src/functions/index.js
@@ -55,10 +55,36 @@ function extractCurrentCheckpointId(inputStr) {
   return parts.join('-');
 }
 
+function deepEqual(obj1, obj2) {
+  if (obj1 === obj2) return true;
+
+  if (
+    obj1 === null ||
+    obj2 === null ||
+    typeof obj1 !== 'object' ||
+    typeof obj2 !== 'object'
+  ) {
+    return false;
+  }
+
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) return false;
+
+  for (let key of keys1) {
+    if (!keys2.includes(key)) return false;
+
+    if (!deepEqual(obj1[key], obj2[key])) return false;
+  }
+
+  return true;
+}
 
 module.exports = {
   convertToCron,
   isArraySubset,
   createArweave,
-  extractCurrentCheckpointId
+  extractCurrentCheckpointId,
+  deepEqual,
 };
--- apps/syncvote/service/node/src/services/VoteHandle/funcs.js
+++ apps/syncvote/service/node/src/services/VoteHandle/funcs.js
@@ -5,7 +5,7 @@ const { supabase } = require('../../configs/supabaseClient');
 
 const moment = require('moment');
 var CronJob = require('cron').CronJob;
-const { createArweave, convertToCron } = require('../../functions');
+const { createArweave, convertToCron, deepEqual } = require('../../functions');
 const PostService = require('../PostService');
 
 const checkIfFirstTimeOfVoting = async (details) => {
@@ -88,7 +88,7 @@ const handleMovingToNextCheckpoint = async (
       }
     }
 
-    // Init next checkpontId
+    // Next checkpontId
     const next_checkpoint_id = `${details.mission_id}-${
       details.children[tallyResult.index]
     }`;
@@ -103,8 +103,15 @@ const handleMovingToNextCheckpoint = async (
     const { data: new_current_vote_data } = await supabase
       .from('current_vote_data')
       .insert({
-        checkpoint_id: next_checkpoint_id,
-        initData: tallyResult,
+        checkpoint_id:
+          next_checkpoint_id + next_checkpoint[0].vote_machine_type ===
+          'ForkNode'
+            ? '-start'
+            : null,
+        initData:
+          next_checkpoint[0].vote_machine_type === 'ForkNode'
+            ? { joinNode: next_checkpoint_id + '-end' }
+            : tallyResult,
         startToVote: startToVote,
       })
       .select('*');
@@ -137,6 +144,14 @@ const handleMovingToNextCheckpoint = async (
       })
       .eq('id', details.mission_id);
 
+    // if next checkpoint is forknode
+    if (
+      details.submission &&
+      next_checkpoint[0].vote_machine_type === 'ForkNode'
+    ) {
+      // create multiple mission in here
+    }
+
     return { next_checkpoint_id };
   } catch (error) {
     console.log('Handle moving to next checkpoint error', error);
@@ -144,34 +159,112 @@ const handleMovingToNextCheckpoint = async (
   }
 };
 
+const handleEndNode = async (details) => {
+  // update mission
+  await supabase.from('mission').eq('id', details.mission_id).update('STOPPED');
+
+  // update current_vote_data
+  await supabase.from('current_vote_data').eq('id', details.cvd_id).update({
+    endedAt: moment().format(),
+  });
+
+  // check if this misison have parent
+  if (details.m_parentId) {
+    // get data of current_vote_data of m_parent
+    const { data } = await supabase
+      .from('mission')
+      .select('*, current_vote_data(*, checkpoint(*))')
+      .eq('id', details.m_parentId);
+    const misison_parent_data = data[0];
+
+    // check if current checkpoint is ForkNode
+    if (
+      misison_parent_data.current_vote_data[0].checkpoint[0]
+        .vote_machine_type === 'ForkNode'
+    ) {
+      // Update current_vote_data of ForkNode
+      let result = misison_parent_data.current_vote_data[0].result;
+      if (result && result.length > 0) {
+        // Append to result mission Id
+        result.push([details.mission_id]);
+      } else {
+        result = [details.mission_id];
+      }
+
+      await supabase
+        .from('current_vote_data')
+        .update({
+          result: result,
+        })
+        .eq('id', misison_parent_data.current_vote_data[0].id);
+
+      // Check if parent misson satisfy the conditions to go to JoinNode
+      if (
+        deepEqual(
+          result,
+          misison_parent_data.current_vote_data[0].initData.start
+        )
+      ) {
+        // Update current_vote_data for ForkNode
+        await supabase
+          .from('current_vote_data')
+          .eq('id', misison_parent_data[0].current_vote_data[0].id)
+          .update({ tallyResult: result, endedAt: moment().format() });
+
+        // Create current_vote_data for JointNode
+        const { data: new_current_vote_data } = await supabase
+          .from('current_vote_data')
+          .insert({
+            checkpoint_id:
+              misison_parent_data.current_vote_data[0].initData.joinNode,
+            startToVote: moment().format(),
+          })
+          .select('*');
+
+        // Update current_vote_data for mission
+        await supabase
+          .from('mission')
+          .eq('id', details.mission_id)
+          .update({ current_vote_data_id: new_current_vote_data[0].id });
+      }
+    } else {
+      console.log(
+        `Children checkpoint is running but mission parent don't run in ForkNode`
+      );
+    }
+  }
+};
+
 module.exports = {
   checkIfFirstTimeOfVoting,
   handleMovingToNextCheckpoint,
+  handleEndNode,
 };
-      // // post when have topic
-      // if (details.topic_id !== null) {
-      //   const { data: web2KeyData, error: errorWeb2KeyData } = await supabase
-      //     .from('web2_key')
-      //     .select('*')
-      //     .eq('org_id', details.org_id);
 
-      //   if (web2KeyData.length > 0) {
-      //     const filteredDiscourse = web2KeyData.filter(
-      //       (integration) => integration.provider === 'discourse'
-      //     );
+// // post when have topic
+// if (details.topic_id !== null) {
+//   const { data: web2KeyData, error: errorWeb2KeyData } = await supabase
+//     .from('web2_key')
+//     .select('*')
+//     .eq('org_id', details.org_id);
 
-      //     if (filteredDiscourse.length === 1) {
-      //       const discourseConfig = filteredDiscourse[0];
+//   if (web2KeyData.length > 0) {
+//     const filteredDiscourse = web2KeyData.filter(
+//       (integration) => integration.provider === 'discourse'
+//     );
 
-      //       const postData = {
-      //         topic_id: details.topic_id,
-      //         raw: `<p>Checkpoint ${next_checkpoint[0].title} has been started</p>
-      //                       <p>Checkpoint description: ${next_checkpoint[0].desc} </p>`,
-      //         org_id: details.org_id,
-      //         discourseConfig,
-      //       };
+//     if (filteredDiscourse.length === 1) {
+//       const discourseConfig = filteredDiscourse[0];
 
-      //       PostService.createPost(postData);
-      //     }
-      //   }
-      // }
+//       const postData = {
+//         topic_id: details.topic_id,
+//         raw: `<p>Checkpoint ${next_checkpoint[0].title} has been started</p>
+//                       <p>Checkpoint description: ${next_checkpoint[0].desc} </p>`,
+//         org_id: details.org_id,
+//         discourseConfig,
+//       };
+
+//       PostService.createPost(postData);
+//     }
+//   }
+// }
