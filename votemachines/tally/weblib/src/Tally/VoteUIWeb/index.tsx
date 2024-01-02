92e57cc2eb4c6f77afa793a7cc2365347577d4d2
onspeedhp <chaukhac4@gmail.com>
2024-01-02 17:55:59 +0700

    Update

--- votemachines/tally/weblib/src/Tally/VoteUIWeb/index.tsx
+++ votemachines/tally/weblib/src/Tally/VoteUIWeb/index.tsx
@@ -4,12 +4,15 @@ import { useSDK } from '@metamask/sdk-react';
 import { ethers } from 'ethers';
 import { Card, Button, Radio, Input } from 'antd';
 import { IVoteUIWebProps } from 'directed-graph';
-import ABI_GOVERNER from '../../utils/abis/OzGovernor_ABI.json';
+import ABI_GOVERNOR from '../../utils/abis/OzGovernor_ABI.json';
+import ABI_TOKEN from '../../utils/abis/ERC20Votes_ABI.json';
+
 const VoteUIWeb = (props: IVoteUIWebProps): JSX.Element => {
   const { onSubmit, checkpointData } = props;
   const [title, setTitle] = useState('');
   const [description, setDiscription] = useState('');
   const [address, setAddress] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const { connected } = useSDK();
 
@@ -19,38 +22,37 @@ const VoteUIWeb = (props: IVoteUIWebProps): JSX.Element => {
         {connected ? (
           <div className='flex flex-col gap-6'>
             <Button
-              onClick={async () => {
-                const addressArray = await window.ethereum.request({
-                  method: 'eth_requestAccounts',
-                });
-
-                console.log(addressArray);
-                
-                setAddress(addressArray[0]);
-              }}
-            >
-              Connect Wallet
-            </Button>
-            <Button
+              loading={loading}
               onClick={async () => {
                 if (window.ethereum) {
-                  const provider = new ethers.providers.Web3Provider(
-                    window.ethereum
-                  );
-                  const signer = provider.getSigner();
+                  setLoading(true);
+                  const provider = new ethers.BrowserProvider(window.ethereum);
 
-                  const governer = new ethers.Contract(
-                    checkpointData?.data.governer,
-                    ABI_GOVERNER,
+                  // const provider = new ethers.providers.Web3Provider(
+                  //   window.ethereum
+                  // );
+                  const signer = await provider.getSigner();
+                  console.log(checkpointData?.data.governor);
+
+                  const governor = new ethers.Contract(
+                    checkpointData?.data.governor,
+                    ABI_GOVERNOR,
                     signer
                   );
-                  let tx = await governer.propose(
-                    checkpointData?.data?.token,
-                    0,
-                    0,
+                  const addressArray = await window.ethereum.request({
+                    method: 'eth_requestAccounts',
+                  });
+
+                  let tx = await governor.propose(
+                    addressArray,
+                    [0],
+                    ['0x'],
                     'Testing create proposal'
                   );
-                  await tx.wait();
+                  const signature = await tx.wait();
+                  console.log('Signature', signature);
+
+                  setLoading(false);
                 }
               }}
             >
