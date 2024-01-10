0f22a679b8b20d19d60c20ef03f383081843950b
onspeedhp <chaukhac4@gmail.com>
2024-01-10 15:57:17 +0700

    Fix metamask error

--- apps/syncvote/webapp/src/main.tsx
+++ apps/syncvote/webapp/src/main.tsx
@@ -5,22 +5,34 @@ import { Provider } from 'react-redux';
 import { store } from '@redux/store';
 import AppRoutes from '@routing';
 import { ConfigProvider } from 'antd';
+import { MetaMaskProvider } from '@metamask/sdk-react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Provider store={store}>
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: '#5D23BB',
-          colorLink: '#5D23BB',
-          colorSuccessBg: '#EAF6EE',
-          colorSuccessText: '#29A259',
-          colorTextDisabled: '#BDA5E3',
-          colorBgContainerDisabled: '#F4F0FA',
-        },
-      }}
-    >
-      <AppRoutes />
-    </ConfigProvider>
-  </Provider>
+  <MetaMaskProvider
+    debug={false}
+    sdkOptions={{
+      checkInstallationImmediately: false,
+      dappMetadata: {
+        name: 'Syncvote',
+        url: window.location.host,
+      },
+    }}
+  >
+    <Provider store={store}>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: '#5D23BB',
+            colorLink: '#5D23BB',
+            colorSuccessBg: '#EAF6EE',
+            colorSuccessText: '#29A259',
+            colorTextDisabled: '#BDA5E3',
+            colorBgContainerDisabled: '#F4F0FA',
+          },
+        }}
+      >
+        <AppRoutes />
+      </ConfigProvider>
+    </Provider>
+  </MetaMaskProvider>
 );
